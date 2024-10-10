95f71dcb6c5a410b884cf5dd8bef0aeeee3fbaa5
rahat-495 <md8702687@gmail.com>
2024-10-10 12:25:23 +0600

    user model are created !

--- Models/UsersModel.js
+++ Models/UsersModel.js
@@ -1,2 +1,74 @@
 
+const mongoose = require("mongoose") ;
 
+const userSchema = new mongoose.Schema({
+    name : {
+        type : String ,
+        required : [true , "Provide Name"] ,
+    },
+    email : {
+        type : String ,
+        required : [true , "Provide Email"] ,
+        unique : true ,
+    },
+    image : {
+        type : String ,
+        default : "" ,
+    },
+    studentUid : {
+        type : String ,
+        required : [true , "Provide StudentUid"] ,
+        unique : true ,
+    },
+    role : {
+        type : String ,
+        default : "student" ,
+    },
+    isBlock : {
+        type : Boolean ,
+        default : false ,
+    },
+    isFired : {
+        type : Boolean ,
+        default : false ,
+    },
+    isjoined : {
+        type : String ,
+        default : "" ,
+    },
+    isjoinedModalSeen : {
+        type : Boolean ,
+        default : false ,
+    },
+    applyForTeacher : {
+        type : String ,
+        default : "No" ,
+    },
+    schools : {
+        type : Array ,
+        default : [] ,
+    },
+    grades : {
+        type : Array ,
+        default : [] ,
+    },
+    removedDevice : {
+        type : Array ,
+        default : [] ,
+    },
+    devicesInfo: {
+        type : [
+            {
+                deviceName : { type: String },
+                loginDate : { type: String },
+                loginTime : { type: String },
+                loginShift : { type: String },
+                isRemoved : { type: Boolean, default: false },
+            }
+        ],
+        default : []
+    },
+})
+
+const UsersModel = mongoose.model("Users" , userSchema) ;
+module.exports = UsersModel ;
