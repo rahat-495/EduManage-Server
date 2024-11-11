7655ab1134b30a1a0ff7d862de99e744655f0f34
rahat-495 <md8702687@gmail.com>
2024-11-11 22:20:16 +0600

    create module route are updated !

--- Controllers/createModule.js
+++ Controllers/createModule.js
@@ -1,12 +1,23 @@
 
-const ModulesModel = require('../Models/ModuleModel')
+const GradesModel = require('../Models/GradesModel');
+const ModulesModel = require('../Models/ModuleModel');
+const UsersModel = require('../Models/UsersModel');
 
 const createModule = async (req , res) => {
     try {
         
         const data = req.body ;
-        const result = await ModulesModel.create(data) ;
-        return res.send(result) ;
+        const isAlreadyUploadModule = await ModulesModel.findOne({grade : data?.grade , subject : data?.subject}) ;
+        if(isAlreadyUploadModule?._id){
+            const result = await ModulesModel.create(data) ;
+            return res.send(result) ;
+        }
+        else{
+            const {totalStudents} = await GradesModel.findOne({_id : data?.grade}).select("totalStudents") ;
+            const result = await ModulesModel.create(data) ;
+            const updateAllUsers = await UsersModel.updateMany({studentUid : { $in : totalStudents }} , { $set : { lastSeenModuleData : `textinstruction/${result?._id}` } } , { strict: false }) ;
+            return res.send(result) ;
+        }
 
     } catch (error) {
         return res.send({message : error.message || error , error : true}) ;
