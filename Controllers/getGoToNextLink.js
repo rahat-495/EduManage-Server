e792472130d51630bcfbd96e16efc2cf22b1140e
rahat-495 <md8702687@gmail.com>
2024-11-16 22:29:37 +0600

    some big validation are added !

--- Controllers/getGoToNextLink.js
+++ Controllers/getGoToNextLink.js
@@ -7,8 +7,9 @@ const getGoToNextLink = async (req , res) => {
         const {moduleId , subject} = req.query ;
         if(moduleId , subject){
             const modules = await ModulesModel.find({subject}).sort({ "createdAt" : 1 }).select("_id") ;
-            const indexOfMyCurrentModule = modules.findIndex((module) => module?._id.toString() === moduleId);
-            return res.send({link : modules[indexOfMyCurrentModule+1]?.id ? 'textinstruction/'+modules[indexOfMyCurrentModule+1]?.id : "noModuleAreCooket"})
+            const indexOfMyCurrentModule = modules.findIndex((module) => module?._id.toString() === moduleId) ;
+            const link = modules[indexOfMyCurrentModule+1]?.id ? 'textinstruction/'+modules[indexOfMyCurrentModule+1]?.id : "noModuleAreCooket" ;
+            return res.send({link , success : modules[indexOfMyCurrentModule+1]?.id ? true : false}) ;
         }
 
     } catch (error) {
--- Controllers/getGoToPrevLink.js
+++ Controllers/getGoToPrevLink.js
@@ -9,12 +9,18 @@ const getGoToPrevLink = async (req , res) => {
             const modules = await ModulesModel.find({subject}).sort({ "createdAt" : 1 }) ;
             const indexOfMyCurrentModule = modules.findIndex((module) => module?._id.toString() === moduleId);
             if(modules[indexOfMyCurrentModule-1]){
-                if(modules[indexOfMyCurrentModule-1]?.moduleData?.length === 3){
-                    return res.send({link : 'videos/'+(modules[indexOfMyCurrentModule-1]?.moduleData[2]?.moduleVideos?.length-1)?.toString()+'/'+modules[indexOfMyCurrentModule-1]?._id+'/'+modules[indexOfMyCurrentModule-1]?.moduleData[2]?.moduleVideos?.at(-1)?.videoName?.split(' ')?.join('_')})
+                if(modules[indexOfMyCurrentModule-1]?.moduleData[2]?.moduleVideos?.length > 0){
+                    return res.send({link : 'videos/'+(modules[indexOfMyCurrentModule-1]?.moduleData[2]?.moduleVideos?.length-1)?.toString()+'/'+modules[indexOfMyCurrentModule-1]?._id+'/'+modules[indexOfMyCurrentModule-1]?.moduleData[2]?.moduleVideos?.at(-1)?.videoName?.split(' ')?.join('_') , success : true}) ;
+                }
+                else if(modules[indexOfMyCurrentModule-1]?.moduleData[1]?.moduleImages?.length > 0){
+                    return res.send({link : 'images/'+(modules[indexOfMyCurrentModule-1]?.moduleData[1]?.moduleImages?.length-1)?.toString()+'/'+modules[indexOfMyCurrentModule-1]?._id+'/'+modules[indexOfMyCurrentModule-1]?.moduleData[1]?.moduleImages?.at(-1)?.imageName?.split(' ')?.join('_') , success : true}) ;
+                }
+                else if(modules[indexOfMyCurrentModule-1]?.moduleData[0]?.textForModuleTitle?.length > 0){
+                    return res.send({link : '/textinstruction/'+modules[indexOfMyCurrentModule-1]?._id , success : true}) ;
                 }
             }
             else{
-                // return res.send({link : modules[indexOfMyCurrentModule+1]?.id ? 'textinstruction/'+modules[indexOfMyCurrentModule+1]?.id : "noModuleAreCooket"})
+                return res.send({link : "no module are previous !" , success : false})
             }
         }
 
