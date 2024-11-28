a3f217a9813529320f5570e80b5e9c87005a6c7c
rahat-495 <md8702687@gmail.com>
2024-11-27 20:42:48 -0800

    updateing prev link is 1/2 is done !

--- Controllers/getGoToPrevLink.js
+++ Controllers/getGoToPrevLink.js
@@ -6,24 +6,44 @@ const getGoToPrevLink = async (req , res) => {
     try {
         
         const {moduleId , subject , studentUid , link} = req.query ;
+        const {lastSeenModuleDatas} = await UsersModel.findOne({studentUid}).select("lastSeenModuleDatas") ;
         if(link && subject && studentUid){
-            const {lastSeenModuleDatas} = await UsersModel.findOne({studentUid}).select("lastSeenModuleDatas") ;
             const updatedLink = lastSeenModuleDatas?.filter((link) => !link?.includes(subject)) ;
             const newLinks = [...updatedLink , subject+'/'+link] ;
-            await UsersModel.updateOne({studentUid} , { $set : { lastSeenModuleDatas : newLinks } } )
+            await UsersModel.updateOne({studentUid} , { $set : { lastSeenModuleDatas : newLinks } } ) ;
+            return res.send({message : "link are updated !" , success : true , reload : true})
         }
         else if(moduleId && subject && !link){
             const modules = await ModulesModel.find({subject}).sort({ "createdAt" : 1 }) ;
             const indexOfMyCurrentModule = modules.findIndex((module) => module?._id.toString() === moduleId);
             if(modules[indexOfMyCurrentModule-1]){
                 if(modules[indexOfMyCurrentModule-1]?.moduleData[2]?.moduleVideos?.length > 0){
-                    return res.send({link : 'videos/'+(modules[indexOfMyCurrentModule-1]?.moduleData[2]?.moduleVideos?.length-1)?.toString()+'/'+modules[indexOfMyCurrentModule-1]?._id+'/'+modules[indexOfMyCurrentModule-1]?.moduleData[2]?.moduleVideos?.at(-1)?.videoName?.split(' ')?.join('_') , success : true}) ;
+
+                    const link = 'videos/'+(modules[indexOfMyCurrentModule-1]?.moduleData[2]?.moduleVideos?.length-1)?.toString()+'/'+modules[indexOfMyCurrentModule-1]?._id+'/'+modules[indexOfMyCurrentModule-1]?.moduleData[2]?.moduleVideos?.at(-1)?.videoName?.split(' ')?.join('_') ;
+
+                    const updatedLink = lastSeenModuleDatas?.filter((link) => !link?.includes(subject)) ;
+                    const newLinks = [...updatedLink , subject+'/'+link] ;
+                    await UsersModel.updateOne({studentUid} , { $set : { lastSeenModuleDatas : newLinks } } ) ;
+
+                    return res.send({link , success : true, reload : true}) ;
                 }
                 else if(modules[indexOfMyCurrentModule-1]?.moduleData[1]?.moduleImages?.length > 0){
-                    return res.send({link : 'images/'+(modules[indexOfMyCurrentModule-1]?.moduleData[1]?.moduleImages?.length-1)?.toString()+'/'+modules[indexOfMyCurrentModule-1]?._id+'/'+modules[indexOfMyCurrentModule-1]?.moduleData[1]?.moduleImages?.at(-1)?.imageName?.split(' ')?.join('_') , success : true}) ;
+                    const link = 'images/'+(modules[indexOfMyCurrentModule-1]?.moduleData[1]?.moduleImages?.length-1)?.toString()+'/'+modules[indexOfMyCurrentModule-1]?._id+'/'+modules[indexOfMyCurrentModule-1]?.moduleData[1]?.moduleImages?.at(-1)?.imageName?.split(' ')?.join('_') ;
+
+                    const updatedLink = lastSeenModuleDatas?.filter((link) => !link?.includes(subject)) ;
+                    const newLinks = [...updatedLink , subject+'/'+link] ;
+                    await UsersModel.updateOne({studentUid} , { $set : { lastSeenModuleDatas : newLinks } } ) ;
+                    
+                    return res.send({link , success : true, reload : true}) ;
                 }
                 else if(modules[indexOfMyCurrentModule-1]?.moduleData[0]?.textForModuleTitle?.length > 0){
-                    return res.send({link : '/textinstruction/'+modules[indexOfMyCurrentModule-1]?._id , success : true}) ;
+                    const link = '/textinstruction/'+modules[indexOfMyCurrentModule-1]?._id ;
+
+                    const updatedLink = lastSeenModuleDatas?.filter((link) => !link?.includes(subject)) ;
+                    const newLinks = [...updatedLink , subject+'/'+link] ;
+                    await UsersModel.updateOne({studentUid} , { $set : { lastSeenModuleDatas : newLinks } } ) ;
+
+                    return res.send({link , success : true, reload : true}) ;
                 }
             }
             else{
